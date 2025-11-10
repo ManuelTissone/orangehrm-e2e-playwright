40762c20fedb479fa37dd178f31aca6b962c8cc6
ManuelTissone <manueltissone94@gmail.com>
2025-11-10 00:53:03 -0300

    Initial setup: Playwright config, structure, .env

---
+++ playwright.config.ts
@@ -0,0 +1,29 @@
+import { defineConfig, devices } from '@playwright/test';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+export default defineConfig({
+  testDir: './tests/e2e',
+  fullyParallel: true,
+  forbidOnly: !!process.env.CI,
+  retries: process.env.CI ? 2 : 0,
+  workers: process.env.CI ? 1 : 2,
+  reporter: 'html',
+  use: {
+    baseURL: process.env.BASE_URL,
+    trace: 'on-first-retry',
+    screenshot: 'only-on-failure',
+  },
+
+  projects: [
+    {
+      name: 'chromium',
+      use: { ...devices.chromiumDesktop },
+    },
+    {
+      name: 'firefox',
+      use: { ...devices.firefox },
+    },
+  ],
+});
